20f5444fbbe340df22bd081e24357a8be1cdd5d5
Voronin <loki2625@yandex.ru>
2024-11-08 11:41:23 +0300

    inter to bastin subnet a & b

--- bastion.tf
+++ bastion.tf
@@ -18,14 +18,27 @@ resource "yandex_compute_instance" "bastion-host" {
 
 
   network_interface {
-    subnet_id          = yandex_vpc_subnet.external_subnet.id
+    subnet_id          = yandex_vpc_subnet.external_subnet_b.id
     nat                = true
     nat_ip_address     = yandex_vpc_address.bastion_address.external_ipv4_address.0.address
     security_group_ids = [yandex_vpc_security_group.secure-port-sg.id]
   }
 
   network_interface {
-    subnet_id          = yandex_vpc_subnet.internal_subnet.id
+    subnet_id          = yandex_vpc_subnet.internal_subnet_b.id
+    nat_ip_address     = "172.16.16.254"
+    security_group_ids = [yandex_vpc_security_group.internal-bastion-sg.id]
+  }
+
+    network_interface {
+    subnet_id          = yandex_vpc_subnet.external_subnet_a.id
+    nat                = true
+    nat_ip_address     = yandex_vpc_address.bastion_address.external_ipv4_address.0.address
+    security_group_ids = [yandex_vpc_security_group.secure-port-sg.id]
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.internal_subnet_a.id
     nat_ip_address     = "172.16.16.254"
     security_group_ids = [yandex_vpc_security_group.internal-bastion-sg.id]
   }
